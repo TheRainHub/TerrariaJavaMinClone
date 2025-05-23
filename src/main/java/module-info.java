6affe361e70a75d2cb008e0ec1c5b0086faf03b9
Mykhailo Plokhin - GitLab <plokhmyk@student.cvut.cz>
2025-05-23 06:17:13 +0200

    edit module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,25 @@
 module org.example.game {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.junit.jupiter.api;
-    requires org.apiguardian.api;
-    exports main;
 
-    opens engine to javafx.fxml;
-    exports engine;
-    exports entity;
-    opens entity to javafx.fxml;
-    exports world;
-    opens world to javafx.fxml;
-    exports util;
-    opens util to javafx.fxml;
+    requires org.apiguardian.api;    // if you annotate your own code with @API
+
+    // expose your packages to others (including the test module):
+    exports main;
+    exports engine.core;
     exports engine.input;
+    exports engine.level;
+    exports engine.save;
+    exports engine.ui;
+    exports entity;
+    exports world;
+    exports util;
+
+    // allow JavaFX to reflectively instantiate controllers, etc.
+    opens main to javafx.fxml;
     opens engine.input to javafx.fxml;
+    opens engine.ui to javafx.fxml;
+    opens entity to javafx.fxml;
+    opens world to javafx.fxml;
+    opens util to javafx.fxml;
 }
